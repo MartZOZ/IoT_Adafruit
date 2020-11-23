ba8b276b09936ee7069e40661100a6aa344bb683
MartZOZ <51233825+MartZOZ@users.noreply.github.com>
2020-11-23 13:51:17 +0100

    Add files via upload

---
+++ Adafruit.py
@@ -0,0 +1,40 @@
+import time
+from Adafruit_IO import Client, Feed, RequestError
+import pyfirmata
+	
+run_count = 0
+ADAFRUIT_IO_USERNAME = "MartZOZ"
+ADAFRUIT_IO_KEY = "aio_idCi59CaCwMM3bzcf337lnkondns"
+
+aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+
+board = pyfirmata.Arduino(’COM5’)
+
+it = pyfirmata.util.Iterator(board)
+it.start()
+
+digital_output = board.get_pin(’d:12:o’)
+
+try:
+	digital = aio.feeds(’digital’)
+
+except RequestError:
+	feed = Feed(name=’digital’)
+	digital = aio.create_feed(feed)
+
+
+while True:
+	print(’Sending count:’, run_count)
+	run_count += 1
+	aio.send_data(’counter’, run_count)
+
+	data = aio.receive(digital.key)
+
+	print(’Data: ’, data.value)
+
+	if data.value == "ON":
+		digital_output.write(True)
+	else:
+		digital_output.write(False)
+
+time.sleep(3)
