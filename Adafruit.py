c98264c0143e28264b3814dc15e1c1dacc842fa6
MartZOZ <51233825+MartZOZ@users.noreply.github.com>
2020-11-27 08:44:35 +0100

    Add files via upload

--- Adafruit.py
+++ Adafruit.py
@@ -4,37 +4,36 @@ import pyfirmata
 	
 run_count = 0
 ADAFRUIT_IO_USERNAME = "MartZOZ"
-ADAFRUIT_IO_KEY = "aio_idCi59CaCwMM3bzcf337lnkondns"
+ADAFRUIT_IO_KEY = "aio_SlEX11t0uBgqkAVWVdAz7iFhwD1P"
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-board = pyfirmata.Arduino(’COM5’)
+board = pyfirmata.Arduino('COM4')
 
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-digital_output = board.get_pin(’d:12:o’)
+digital_output = board.get_pin('d:3:o')
 
 try:
-	digital = aio.feeds(’digital’)
+	digital = aio.feeds('digital')
 
 except RequestError:
-	feed = Feed(name=’digital’)
+	feed = Feed(name='digital')
 	digital = aio.create_feed(feed)
 
-
 while True:
-	print(’Sending count:’, run_count)
+	print('Sending count:', run_count)
 	run_count += 1
-	aio.send_data(’counter’, run_count)
+	aio.send_data('counter', run_count)
 
 	data = aio.receive(digital.key)
 
-	print(’Data: ’, data.value)
+	print('Data: ', data.value)
 
 	if data.value == "ON":
 		digital_output.write(True)
 	else:
 		digital_output.write(False)
 
-time.sleep(3)
+time.sleep(15)
