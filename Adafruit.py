13996f68c239679df3f1544f944a0f55e082d5c7
MartZOZ <51233825+MartZOZ@users.noreply.github.com>
2020-11-27 08:55:39 +0100

    Add files via upload

--- Adafruit.py
+++ Adafruit.py
@@ -4,37 +4,37 @@ import pyfirmata
 	
 run_count = 0
 ADAFRUIT_IO_USERNAME = "MartZOZ"
-ADAFRUIT_IO_KEY = "aio_idCi59CaCwMM3bzcf337lnkondns"
+ADAFRUIT_IO_KEY = "aio_MiRh54jyRgfsySEv6XAkdNX8qtQH"
+#Adafruit IO keys regenerate semi-regularly, make sure to check if it is active!
 
 aio = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 
-board = pyfirmata.Arduino(’COM5’)
+board = pyfirmata.Arduino('COM4')
 
 it = pyfirmata.util.Iterator(board)
 it.start()
 
-digital_output = board.get_pin(’d:12:o’)
+digital_output = board.get_pin('d:3:o')#LED pin on arduino
 
 try:
-	digital = aio.feeds(’digital’)
+	digital = aio.feeds('digital') #This requests the feed from the feed "digital"
 
 except RequestError:
-	feed = Feed(name=’digital’)
+	feed = Feed(name='digital') #
 	digital = aio.create_feed(feed)
 
-
 while True:
-	print(’Sending count:’, run_count)
+	print('Sending count:', run_count)
 	run_count += 1
-	aio.send_data(’counter’, run_count)
+	aio.send_data('counter', run_count) #This checks and updates the counter value every time the code checks for an update from the "counter" feed
 
 	data = aio.receive(digital.key)
 
-	print(’Data: ’, data.value)
+	print('Data: ', data.value)
 
-	if data.value == "ON":
+	if data.value == "ON": #This checks if the toggle key on adafruit is On or Off
 		digital_output.write(True)
 	else:
 		digital_output.write(False)
 
-time.sleep(3)
+time.sleep(15)
